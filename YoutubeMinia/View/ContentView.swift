8029c20c91e7938d5d1f598669d8b9816da77128
Nicolas Bachur <hello@nbapps.fr>
2024-04-28 17:39:14 +0200

    remove all delivered notifs on scene phase change

--- YoutubeMinia/View/ContentView.swift
+++ YoutubeMinia/View/ContentView.swift
@@ -8,28 +8,34 @@
 import SwiftUI
 import SwiftData
 import Combine
+import UserNotifications
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.scenePhase) private var scenePhase
 
     @EnvironmentObject private var viewModel: ThumbnailMakerViewModel
     
     var body: some View {
+        Group {
 #if os(macOS)
-        NavigationSplitView {
-            LastUrlsListView()
-                .navigationSplitViewColumnWidth(min: 200, ideal: 200, max: 300)
-        } detail: {
-            ThumbnailMakerView()
-                .navigationTitle("!Youtube Minia Maker")
-        }
+            NavigationSplitView {
+                LastUrlsListView()
+                    .navigationSplitViewColumnWidth(min: 200, ideal: 200, max: 300)
+            } detail: {
+                ThumbnailMakerView()
+                    .navigationTitle("!Youtube Minia Maker")
+            }
 #else
-        NavigationStack {
-            ThumbnailMakerView()
-                .navigationTitle("!Youtube Minia Maker")
-        }
+            NavigationStack {
+                ThumbnailMakerView()
+                    .navigationTitle("!Youtube Minia Maker")
+            }
 #endif
-        
+        }
+        .onChange(of: scenePhase) { _, _ in
+            UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        }
     }
 }
 
